306ee8b32bba06809c6c76886f10628605f39ecb
KaloyanYosifov <kaloqn665@gmail.com>
2019-05-12 12:47:42 +0300

    Add function to check if binary exists

--- start-android.sh
+++ start-android.sh
@@ -1,8 +1,18 @@
 #! /bin/bash
 
-abdLocation=$(which adb 2> /dev/null)
+checkIfbinaryExists()
+{
+    # get the binary location
+    # and send the errors (2>) to null
+    binaryLocation=$(which $1 2> /dev/null)
+    
+    if [ -z $binaryLocation ]; then
+        echo "$2"
+        exit 0
+    fi
+}
 
-if [ -z $abdLocation ]; then
-    echo "Please install Android Studio or (add platform-tools to your \$PATH environement)!"
-    exit 0
-fi
+# get the lcoation of abd binary of android studio
+checkIfbinaryExists "android" "Please install Android Studio!"
+
+android=$(which android)
