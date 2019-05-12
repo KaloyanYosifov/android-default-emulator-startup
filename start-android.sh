7b439dac6fd868d9a1f33b86db581ea64227ffb2
KaloyanYosifov <kaloqn665@gmail.com>
2019-05-12 13:12:30 +0300

    Add a check if the daemon adb server is started

--- start-android.sh
+++ start-android.sh
@@ -22,4 +22,19 @@ checkIfbinaryExists "emulator" "Please include the '\$ANDROID_HOME\\emulator' in
 checkIfbinaryExists "adb" "Please include the '\$ANDROID_HOME\\platform-tools' in your '\$PATH' environment!"
 
 # get the basename of the device we are going to use
+# this is still work in progress
+# TODO check if we have multiple devices how is this command going to handle it AND find a better way to get the devices name
+echo "Getting Device Name"
 deviceBaseName=$(basename $(avdmanager list avd | grep Path: | sed "s/Path\:/ /g") ".avd")
+
+# empty line
+echo
+
+# check if there is a process where adb is running it
+# if the if statement is empty it means no pid is found with that command name
+# if not we start the server
+
+if [ -z $(ps -C adb --no-headers -o pid) ]; then
+    echo "Starting adb daemon server!"
+    $(adb start-server)
+fi
